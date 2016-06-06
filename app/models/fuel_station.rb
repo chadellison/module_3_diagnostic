4057a1792de8dceac022643d5a456e8ede6982d7
Chad Ellison <chad.ellison0123@gmail.com>
2016-06-06 10:42:26 -0600

    Api needs to be parsed differently

--- app/models/fuel_station.rb
+++ app/models/fuel_station.rb
@@ -1,10 +1,10 @@
 class FuelStation < OpenStruct
-  def service
+  def self.service
     @service = FuelStationService.new
   end
 
   def self.nearest_fuel_stations(params)
-    @service.fuel_stations_hash(params).map do |fuel_station|
+    self.service.fuel_stations_hash(params).map do |fuel_station|
       FuelStation.new(fuel_station)
     end
   end
